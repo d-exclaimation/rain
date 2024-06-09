2d407d9e646370937446a95ab8bd3dba3dbd41bc
Vincent <thisoneis4business@gmail.com>
2024-06-09 19:40:36 +1200

    light mode > dark mode

--- src/Rain.svelte
+++ src/Rain.svelte
@@ -12,14 +12,14 @@
 
 <div class="flex items-center justify-center w-full h-full">
   <div
-    class="absolute z-10 w-60 h-60 md:w-80 md:h-80 overflow-clip -translate-x-1/3 blur-2xl bg-fuchsia-400/20 md:bg-fuchsia-400/10 rounded-full [-webkit-transform:translate3d(var(--tw-translate-x),0,0)]"
+    class="absolute z-10 w-48 h-48 md:w-80 md:h-80 overflow-clip -translate-x-1/3 blur-2xl bg-fuchsia-400/40 md:bg-fuchsia-400/30 rounded-full [-webkit-transform:translate3d(var(--tw-translate-x),0,0)]"
   ></div>
   <div
-    class="absolute z-10 w-60 h-60 md:w-80 md:h-80 overflow-clip translate-x-1/3 blur-2xl bg-blue-400/20 md:bg-blue-400/10 rounded-full [-webkit-transform:translate3d(var(--tw-translate-x),0,0)]"
+    class="absolute z-10 w-48 h-48 md:w-80 md:h-80 overflow-clip translate-x-1/3 blur-2xl bg-blue-400/40 md:bg-blue-400/30 rounded-full [-webkit-transform:translate3d(var(--tw-translate-x),0,0)]"
   ></div>
   {#if !!$query.data}
     <div
-      class="absolute text-white text-xl md:text-5xl font-comfort max-w-[40rem] text-balance"
+      class="absolute text-black text-2xl md:text-5xl font-medium font-comfort max-w-[40rem] text-balance"
       transition:fade={{ delay: 250, duration: 300 }}
     >
       {$query.data}
